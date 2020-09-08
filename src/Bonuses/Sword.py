2db36c91cfb50ad95259ea657ce4acb25d41b382
BossOfCreeps <seva-ra4is@mail.ru>
2020-09-08 15:20:37 +0700

    Add files via upload

---
+++ src/Bonuses/Sword.py
@@ -0,0 +1,16 @@
+from random import randint
+
+from Bonus import Bonus
+from constants import sword_max_value
+
+
+class Sword(Bonus):
+    def __init__(self, max_value=sword_max_value):
+        super().__init__()
+
+        # Check value
+        if max_value > sword_max_value:
+            raise Exception("Sword can't get so much power")
+
+        # Set value
+        self._value = randint(1, max_value)
